0d75cf264f452e117f68109b529f83d2f9d914ba
DongHyeon KANG <knycrew@naver.com>
2023-08-02 14:08:47 +0900

    T1 TEST

--- T1/study1.js
+++ T1/study1.js
@@ -2,7 +2,7 @@ const http = require('http');
 //
 
 const hostname = '127.0.0.1';
-const port = '30000';
+const port = '3000';
 
 const server = http.createServer(function(req,res){
     res.statusCode = '200';
