f235cedaaf06c0606cf1de994fc3ac80de696674
DongHyeon KANG <knycrew@naver.com>
2023-08-02 13:39:41 +0900

    second commit

---
+++ nodejs/study1.js
@@ -0,0 +1,18 @@
+const http = require('http');
+//
+
+const hostname = '127.0.0.1';
+const port = '3000';
+
+const server = http.createServer(function(req,res){
+    res.statusCode = '200';
+    res.end('hello world');
+
+
+});
+
+server.listen(port,hostname, () => {
+
+    console.log('Server running at http://${hostname}:${port}/');
+
+});
